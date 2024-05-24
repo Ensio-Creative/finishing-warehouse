c351d3660409df49b1306ee91c1d0772fef02e46
alabo-excel <iamalaboexcel@gmail.com>
2024-05-24 05:57:00 +0100

    contact form commit

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,58 @@
+<script setup>
+import { ref } from "vue";
+
+import { createToast } from "mosha-vue-toastify";
+import "mosha-vue-toastify/dist/style.css";
+
+const fName = ref("");
+const lName = ref("");
+const message = ref("");
+const email = ref("");
+const phone = ref("");
+const loading = ref(false);
+
+const handleFormSubmit = (e) => {
+  // e.preventDefault();
+  loading.value = true;
+
+  fetch("https://formsubmit.co/ajax/info@finishingwarehouse.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: fName.value + lName.value,
+      message: message.value,
+      email: email.value,
+      phone: phone.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.success === "true") {
+        createToast("Message sent successfully!");
+        loading.value = false;
+        email.value = "";
+        message.value = "";
+        phone.value = "";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      createToast("An error occured, Please try again", error.text);
+      loading.value = false;
+
+      email.value = "";
+      message.value = "";
+      phone.value = "";
+      loading.value = false;
+    });
+};
+</script>
+
+
 <template>
 
   <Head>
@@ -47,29 +102,30 @@
         <div class="lg:w-[70%] bg-[#F0F0F0] lg:p-10 p-4 lg:mt-0 mt-4 rounded-xl">
           <div class="lg:flex lg:my-4 justify-between">
             <div class="lg:w-[48%]">
-              <input type="text" placeholder="First Name*"
+              <input v-model="fName" type="text" placeholder="First Name*"
                 class="w-full bg-transparent border-b border-[#84240C] p-4 focus:outline-none text-[#0F0F0F] text-sm">
             </div>
             <div class="lg:w-[48%]">
-              <input type="text" placeholder="Last Name*"
+              <input v-model="lName" type="text" placeholder="Last Name*"
                 class="w-full bg-transparent border-b border-[#84240C] p-4 focus:outline-none text-[#0F0F0F] text-sm">
             </div>
           </div>
           <div class="lg:flex lg:my-6 justify-between">
             <div class="lg:w-[48%]">
-              <input type="text" placeholder="Email Address"
+              <input v-model="email" type="text" placeholder="Email Address"
                 class="w-full bg-transparent border-b border-[#84240C] p-4 focus:outline-none text-[#0F0F0F] text-sm">
             </div>
             <div class="lg:w-[48%]">
-              <input type="text" placeholder="Phone Number*"
+              <input v-model="phone" type="text" placeholder="Phone Number*"
                 class="w-full bg-transparent border-b border-[#84240C] p-4 focus:outline-none text-[#0F0F0F] text-sm">
             </div>
           </div>
           <div class="my-8">
-            <textarea placeholder="Your Message*"
+            <textarea v-model="message" placeholder="Your Message*"
               class="p-4 text-sm border text-[#0F0F0F] rounded-xl border-[#84240C] h-72 focus:outline-none w-full bg-transparent"></textarea>
           </div>
-          <button class="bg-[#84240C] rounded-xl text-white w-full p-3">SEND MESSAGE</button>
+          <button @click="handleFormSubmit(e)" class="bg-[#84240C] rounded-xl text-white w-full p-3">{{ loading ?
+                "Loading..." : 'SEND MESSAGE' }}</button>
         </div>
       </div>
     </div>
