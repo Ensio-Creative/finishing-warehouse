d4320304df53d769c54d34a5145bb106cee6638c
alabo-excel <iamalaboexcel@gmail.com>
2026-06-13 23:01:14 +0100

    fix: project page commit

--- pages/projects.vue
+++ pages/projects.vue
@@ -5,33 +5,46 @@
   </Head>
   <div>
     <HeroComp img="projects" text="Projects" />
-    <div class="lg:px-20 px-4 lg:py-20 py-12">
-      <!-- <p class="lg:text-4xl  font-semibold mb-4 text-3xl">Projects</p> -->
-      <div class="">
-        <div class="lg:w-[70%] mb-8">
-          At Finishing Warehouse, we also specialize in undertaking projects for house finishing. Whether you're
-          building a new home or renovating an existing space, our experienced team can handle all aspects of the
-          finishing process, from design consultation to product installation.
-          <br> <br>
-          At Finishing Warehouse, sustainability is at the core of everything we do. When you choose us for your house
-          finishing project, you can trust that we will prioritize
-          eco-friendly materials and sustainable practices every step of the way.
-          <br> <br>
-
-          With our expertise and attention to detail, you can trust us to deliver superior results that exceed your
-          expectations. Contact us today to discuss your project requirements and let us bring your vision to life.
-          we can.
-          <br> <br>
-
-          Below are some of the projects we have executed.
+    <section>
+      <div class="lg:px-20 px-4 lg:py-20 py-12">
+        <p class="lg:text-[35px] text-[#84240C] font-medium text-2xl lg:mb-0 mb-4"> Bathroom</p>
+        <div class="grid lg:grid-cols-3 gap-4 mt-8">
+          <div v-for="value in 3" :key="value">
+            <img :src="`/images/projects/bathroom-${value}.jpg`" alt="" />
+          </div>
         </div>
-        <img class="mb-8" src="/images/projects/projects1.png" alt="">
-
-        <img class="mb-8 lg:ml-auto" src="/images/projects/projects4.png" alt="">
-        <img class="mb-8" src="/images/projects/projects3.png" alt="">
-        <img class="lg:ml-auto" src="/images/projects/projects2.png" alt="">
       </div>
-    </div>
+      <div class="border-b border-[#AFAFAF]"></div>
 
+      <div class="lg:px-20 px-4 lg:py-20 py-12">
+        <p class="lg:text-[35px] text-[#84240C] font-medium text-2xl lg:mb-0 mb-4"> Kitchen</p>
+        <div class="grid lg:grid-cols-3 gap-4 mt-8">
+          <div v-for="value in 3" :key="value">
+            <img :src="`/images/projects/kitchen-${value}.jpg`" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="border-b border-[#AFAFAF]"></div>
+
+      <div class="lg:px-20 px-4 lg:py-20 py-12">
+        <p class="lg:text-[35px] text-[#84240C] font-medium text-2xl lg:mb-0 mb-4"> Sitting Room</p>
+        <div class="grid lg:grid-cols-3 gap-4 mt-8">
+          <div v-for="value in 3" :key="value">
+            <img :src="`/images/projects/sitting-room-${value}.jpg`" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="border-b border-[#AFAFAF]"></div>
+
+      <div class="lg:px-20 px-4 lg:py-20 py-12">
+        <p class="lg:text-[35px] text-[#84240C] font-medium text-2xl lg:mb-0 mb-4">Washbasin</p>
+        <div class="grid lg:grid-cols-3 gap-4 mt-8">
+          <div v-for="value in 3" :key="value">
+            <img :src="`/images/projects/washbasin-${value}.jpg`" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="border-b border-[#AFAFAF]"></div>
+    </section>
   </div>
 </template>
